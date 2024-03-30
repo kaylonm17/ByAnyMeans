c8bcf2fb7418c62a99566dfc2f7eea799ef8acb1
kaylonm17 <you@example.com>
2024-03-29 20:14:09 -0500

    Creating pallettes in theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,1 +1,8 @@
-// color palettes inferno redish and purpleish, nocturnal purples and greens,
+// color palettes inferno redish and purpleish, nocturnal purples and greens, aqua slush bluey, earth green and brown, shocking
+
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+
+implort { createContext}
+// color design tokens
+export const tokens = 
