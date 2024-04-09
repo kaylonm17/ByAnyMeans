3df147ae3fa25acda98c2361840918955dbbdc75
kaylonm17 <you@example.com>
2024-04-09 00:26:12 -0500

    created routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Form from "./scenes/form";
+import FAQ from "./scenes/faq";
+import Calendar from "./scenes/calendar";
+import Todo from "./scenes/todo";
+import Signin from "./scenes/signin";
+import Feedback from "./scenes/feedback";
+import Chat from "./scenes/chat";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+function App () {
+  const[theme, colorMode] = useMode();
+
+  return (<ColorModeContext.Provider value={colorMode}>
+    <ThemeProvier theme={theme}>
+      <CssBaseline />
+      <div calssName="app">
+        <Sidebar/>
+        <main className="content">
+          <Topbar/>
+          <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+            <Route path="/team" element={<Team/>}/>
+            <Route path="/contacts" element={<Contacts/>}/>
+            <Route path="invoices/" element={<Invoices/>}/>
+            <Route path="/form" element={<Form/>}/>
+            <Route path="/faq" element={<FAQ/>}/>
+            <Route path="/calendar" element={<Calendar/>}/>
+            <Route path="/todo" element={<Todo/>}/>
+            <Route path="/signin" element={<Signin/>}/>
+            <Route path="/feedback" element={<Feedback/>}/>
+            <Route path="/chat" element={<Chat/>}/>
+          </Routes>
+        </main>
+      </div>
+    </ThemeProvier>
+  </ColorModeContext.Provider>
   );
 }
 
