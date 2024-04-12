7c436608e7d180debd49974c263974d69ce2b1fc
kaylonm17 <you@example.com>
2024-04-12 01:39:21 -0500

    app.js

--- src/App.js
+++ src/App.js
@@ -29,16 +29,17 @@ function App () {
           <Topbar/>
           <Routes>
             <Route path="/" element={<Dashboard/>}/>
-            <Route path="/team" element={<Team/>}/>
-            <Route path="/contacts" element={<Contacts/>}/>
-            <Route path="invoices/" element={<Invoices/>}/>
-            <Route path="/form" element={<Form/>}/>
-            <Route path="/faq" element={<FAQ/>}/>
-            <Route path="/calendar" element={<Calendar/>}/>
-            <Route path="/todo" element={<Todo/>}/>
-            <Route path="/signin" element={<Signin/>}/>
-            <Route path="/feedback" element={<Feedback/>}/>
-            <Route path="/chat" element={<Chat/>}/>
+            {/* <Route path="/team" element={<Team/>}/> */}
+            {/* <Route path="/contacts" element={<Contacts/>}/> */}
+            {/* <Route path="invoices/" element={<Invoices/>}/> */}
+            {/* <Route path="/form" element={<Form/>}/> */}
+            {/* <Route path="/faq" element={<FAQ/>}/> */}
+            {/* <Route path="/calendar" element={<Calendar/>}/> */}
+            {/* <Route path="/todo" element={<Todo/>}/> */}
+            {/* <Route path="/signin" element={<Signin/>}/> */}
+            {/* <Route path="/feedback" element={<Feedback/>}/> */}
+            {/* <Route path="/chat" element={<Chat/>}/> */}
+            {/* <Route path="/notes" element={<Notes/>}/> */}
           </Routes>
         </main>
       </div>
